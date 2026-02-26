04e8856fc2b704fca1f6766a597d5b1b1863f2d8
dantheman20245 <dantheman20245@gmail.com>
2026-02-26 09:51:43 -0800

    ui start
    ui start

---
+++ Assets/Scripts/UI Controllers/Daniel/UIController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace YourName
+{
+public class UIController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+}
